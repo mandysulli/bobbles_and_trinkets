36edae774abf692bda9468d6da9c4ac8ff08e23e
mandysulli <yxpa3@cdc.gov>
2026-01-22 10:01:39 -0500

    run mira-oxide in shell script

---
+++ shell_scripts/run_mira-oxide.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#$ -o mira-oxide.$JOB_ID.out
+#$ -e mira-oxide.$JOB_ID.err
+#$ -N mira-oxide_singularity
+#$ -pe smp 4
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=50G
+#$ -q highmem.q
+#$ -cwd
+#$ -V
+
+module load singularity
+
+WORKDIR=/scicomp/groups/OID/NCIRD/ID-OD/VSDB/BIA/FLU_SC2_SEQUENCING/sra-2026-01-15
+
+cd "$WORKDIR" || exit 1
+
+echo "Running MIRA-Oxide prepare-mira-reports command"
+echo "Working directory: $PWD"
+
+singularity exec \
+    --bind "$WORKDIR":"$WORKDIR":rw \
+    --bind /scicomp/groups/OID/NCIRD/ID-OD/VSDB/BIA/MIRA-NF:/scicomp/groups/OID/NCIRD/ID-OD/VSDB/BIA/MIRA-NF:ro \
+    mira-oxide_v1.3.1.sif \
+    mira-oxide prepare-mira-reports \
+    -s "$WORKDIR/samplesheet.csv" \
+    -i "$WORKDIR/mira-output" \
+    -o "$WORKDIR/mira-output/manual_mira_oxide_run" \
+    -q /scicomp/groups/OID/NCIRD/ID-OD/VSDB/BIA/MIRA-NF/bin/irma_config/qc_pass_fail_settings.yaml \
+    -p illumina \
+    -w /scicomp/groups/OID/NCIRD/ID-OD/VSDB/BIA/MIRA-NF \
+    -r sra-2026-01-15 \
+    -v flu \
+    -f
