46df55d070a28ef30f8940e3fce941d417227b32
mandysulli <yxpa3@cdc.gov>
2026-01-22 14:37:59 -0500

    updates

--- shell_scripts/run_mira-oxide.sh
+++ shell_scripts/run_mira-oxide.sh
@@ -4,7 +4,7 @@
 #$ -N mira-oxide_singularity
 #$ -pe smp 4
 #$ -l h_rt=24:00:00
-#$ -l h_vmem=50G
+#$ -l h_vmem=200G
 #$ -q highmem.q
 #$ -cwd
 #$ -V
@@ -14,6 +14,10 @@ module load singularity
 WORKDIR=/scicomp/groups/OID/NCIRD/ID-OD/VSDB/BIA/FLU_SC2_SEQUENCING/sra-2026-01-15
 
 cd "$WORKDIR" || exit 1
+
+singularity pull docker:cdcgov/mira-oxide:v1.3.1
+
+cp /scicomp/groups/OID/NCIRD/ID-OD/VSDB/BIA/FLU_SC2_SEQUENCING/sra-2026-01-15/mira-output/aggregate_outputs/dais-ribosome/DAIS_ribosome.seq mira-output/manual_mira_oxide_run
 
 echo "Running MIRA-Oxide prepare-mira-reports command"
 echo "Working directory: $PWD"
